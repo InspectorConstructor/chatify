517102e03c9d25936ab1ad9f415ff23e00b25616
Nicholas St.Pierre <nico200@gmail.com>
2014-12-05 22:09:05 -0500

    whoever put nonstandard quotes on that code snippet is a level seven moron

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -5,4 +5,4 @@
    create a bookmark and paste this into it
  */
 
-(function(){var loader=document.createElement(”script”);loader.type=“text/javascript”;loader.src='https://raw.githubusercontent.com/InspectorConstructor/chatify/master/src/chatify.js';})();
+(function(){var loader=document.createElement('script');loader.type='text/javascript';loader.src='https://raw.githubusercontent.com/InspectorConstructor/chatify/master/src/chatify.js';})();
